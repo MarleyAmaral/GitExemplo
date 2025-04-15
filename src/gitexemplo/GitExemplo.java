a03a480fa6573ef6817a927a0b4f2c27d30f2b68
Marley Amaral Oliveira <marleyct390@gmail.com>
2025-04-15 01:01:23 -0300

    Hello World

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -4,17 +4,12 @@
  */
 package gitexemplo;
 
-/**
- *
- * @author User
- */
 public class GitExemplo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Hello World");
     }
+}  
     
-}
+    
+
